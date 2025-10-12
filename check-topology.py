7f769ba8c692ba01965b49f8b7106397a5fa743b
xiangdong <40376367+zxd1997066@users.noreply.github.com>
2025-10-12 19:22:00 +0800

    Update check-topology.py

--- check-topology.py
+++ check-topology.py
@@ -5,6 +5,7 @@ import sys
 ret = os.system("xpu-smi topology -m 2>&1|tee topology.log")
 if ret == 0:
     gpu_dict = {}
+    cpu_dict = {}
     with open("topology.log") as file:
         lines = file.readlines()
         for line in lines:
@@ -28,28 +29,28 @@ if ret == 0:
                         else:
                             affinity = affinity + "," + str(j - 2)
                 gpu_dict[i] = affinity
-        print(gpu_dict)
+                cpu_dict[i] = cpu_affinity
 
     max_affinity = ""
     value_to_keys = {}
+    gpu_cpu_dict = {}
     for key, value in gpu_dict.items():
         if value not in value_to_keys:
             value_to_keys[value] = []
         value_to_keys[value].append(key)
+    dist_group = []
     for key, value in value_to_keys.items():
-        print(key == ','.join(value_to_keys[key]))
-
-    os.environ["ZE_AFFINITY_MASK"] = str(max_affinity)
-    print(str("ZE_AFFINITY_MASK=" + os.environ.get("ZE_AFFINITY_MASK")))
-
+        if key == ','.join(value_to_keys[key]):
+            dist_group.append(key)
+    for group in dist_group:
+        cpu_aff = []
+        for i in group.split(","):
+            if cpu_dict[i] not in cpu_aff:
+                cpu_aff.append(cpu_dict[i])
+        if len(cpu_aff) == 1:
+            gpu_cpu_dict[group] = ','.join(cpu_aff)
+    print(gpu_cpu_dict)
 else:
     print("xpu-smi topology failed")
 
     sys.exit(255)
-
-
-
-
-
-
-
